664aa86e1d3f9ecf06bf55432595832bf7d37336
Humorloos <l.loos95@gmail.com>
2021-08-12 17:40:21 +0200

    Stop old watches before creating new ones

--- setup_watches.py
+++ setup_watches.py
@@ -11,30 +11,37 @@ from googleApiClientProvider import GoogleApiClientProvider
 from utils import get_calendar_ids
 
 SCOPES = [googleApiScopes.calendar.EVENTS, googleApiScopes.calendar.CALENDAR_READ_ONLY]
-WATCH_DURATION = str(int(dt.timedelta(minutes=2).total_seconds()))
+WATCH_DURATION = str(int(dt.timedelta(days=1, hours=6).total_seconds()))
 CALENDAR_LOOKUP_PATH = Path('resources').joinpath('calendar_lookup.pickle')
 
 client_provider = GoogleApiClientProvider(SCOPES)
-
 calendar_service = client_provider.get_service(name="calendar", version='v3')
 
+# Close old channels
+with open(CALENDAR_LOOKUP_PATH, 'rb') as in_file:
+    previous_channels = pickle.load(in_file)
+
+for channel_id, id_dict in previous_channels.items():
+    calendar_service.channels().stop(body={'id': channel_id, 'resourceId': id_dict['resource_id']})
+
+# Open new channels
 calendar_ids = get_calendar_ids(calendar_service)
+calendar_lookup = {str(uuid.uuid1()): {'calendar_id': calendar_id} for calendar_id in calendar_ids}
 
-calendar_lookup = {str(uuid.uuid1()): calendar_id for calendar_id in calendar_ids}
-
-for channel_id, calendar_id in calendar_lookup.items():
-    calendar_service.events().watch(
-        calendarId=calendar_id,
-        body={
-            "id": channel_id,
-            "token": "my token",
-            "type": "web_hook",
-            "address": "https://humorloos.pythonanywhere.com/",
-            "params": {
-                "ttl": WATCH_DURATION
-            }
+responses = [calendar_service.events().watch(
+    calendarId=id_dict['calendar_id'],
+    body={
+        "id": channel_id,
+        "token": "my token",
+        "type": "web_hook",
+        "address": "https://humorloos.pythonanywhere.com/",
+        "params": {
+            "ttl": WATCH_DURATION
         }
-    ).execute()
+    }
+).execute() for channel_id, id_dict in calendar_lookup.items()]
 
+for response in responses:
+    calendar_lookup[response['id']]['resource_id'] = response['resourceId']
 with open(CALENDAR_LOOKUP_PATH, 'wb') as out_file:
     pickle.dump(calendar_lookup, out_file)
