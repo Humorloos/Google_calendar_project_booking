84adce6943b3d40fef7394616aa9b9244ba33b1a
Humorloos <l.loos95@gmail.com>
2021-08-27 13:36:20 +0200

    Refactor filter for confirmed updates

--- calendar_handler.py
+++ calendar_handler.py
@@ -66,85 +66,85 @@ class CalendarHandler(Resource):
         )
 
         if len(updated_events) > 0 and 'updated' in updated_events.columns:
-            updated_events = updated_events.sort_values(by='updated', ascending=False, ignore_index=True)
+            updated_events = updated_events[updated_events['status'] == 'confirmed'].sort_values(
+                by='updated', ascending=False, ignore_index=True)
             # remember projects that were already updated to update each project only once
             updated_projects = set()
             for _, updated_event in updated_events.iterrows():
-                if updated_event['status'] == 'confirmed':
-                    # only trigger updates for events that belong to a project that hasn't been updated so far
-                    if updated_event['summary'].endswith(PROJECT_SUFFIX) and \
-                            updated_event['summary'] not in updated_projects:
+                # only trigger updates for events that belong to a project that hasn't been updated so far
+                if updated_event['summary'].endswith(PROJECT_SUFFIX) and \
+                        updated_event['summary'] not in updated_projects:
 
-                        # get events that belong to this project
-                        project_events, sync_token = calendar_service.get_event_df_and_next_sync_token(
-                            calendar_id=target_calendar_id,
-                            query=updated_event['summary'][:-3],
-                        )
+                    # get events that belong to this project
+                    project_events, sync_token = calendar_service.get_event_df_and_next_sync_token(
+                        calendar_id=target_calendar_id,
+                        query=updated_event['summary'][:-3],
+                    )
 
-                        # update all events in this project except the event that triggered the update
-                        new_description = updated_event['description']
-                        for _, project_event in project_events.iterrows():
-                            if project_event['id'] != updated_event['id'] and \
-                                    project_event['summary'] == updated_event['summary'] and \
-                                    project_event['description'] != new_description:
-                                project_event['description'] = new_description
-                                calendar_service.service.events().update(
-                                    calendarId=target_calendar_id,
-                                    eventId=project_event['id'],
-                                    body=event_row_to_body(project_event),
-                                ).execute()
-
-                        updated_projects.add(updated_event['summary'])
-                    if 'colorId' in updated_event.index and updated_event['colorId'] == SPLIT_COLOR_ID:
-                        end_timestamp = calendar_service.local_datetime_from_string(updated_event['end']['dateTime'])
-                        feierabend_timestamp = calendar_service.get_local_datetime(end_timestamp.date(), FEIERABEND)
-                        split_timestamp = None
-                        if end_timestamp > feierabend_timestamp:
-                            split_timestamp = feierabend_timestamp
-                        else:
-                            interrupting_events = pd.DataFrame()
-                            for calendar_id in calendar_lookup['calendar_id'].values:
-                                events_to_append, sync_token = calendar_service.get_event_df_and_next_sync_token(
-                                    calendar_id=calendar_id,
-                                    time_max=updated_event['end']['dateTime'],
-                                    time_min=updated_event['start']['dateTime'],
-                                )
-                                interrupting_events = interrupting_events.append(events_to_append)
-                            interrupting_events = interrupting_events[
-                                (interrupting_events['id'] != updated_event['id']) &
-                                (interrupting_events['start'].apply(lambda start_dict: 'dateTime' in start_dict.keys()))
-                                ]
-                            if len(interrupting_events) > 0:
-                                split_timestamp = interrupting_events['start'].apply(
-                                    calendar_service.extract_local_datetime_or_nat).min()
-                        if split_timestamp is not None:
-                            duration_to_trim = end_timestamp - split_timestamp
-
-                            # update event to end at split timestamp
-                            updated_event = event_row_to_body(updated_event)
-                            del updated_event['colorId']
-                            updated_event['end']['dateTime'] = split_timestamp.isoformat()
+                    # update all events in this project except the event that triggered the update
+                    new_description = updated_event['description']
+                    for _, project_event in project_events.iterrows():
+                        if project_event['id'] != updated_event['id'] and \
+                                project_event['summary'] == updated_event['summary'] and \
+                                project_event['description'] != new_description:
+                            project_event['description'] = new_description
                             calendar_service.service.events().update(
                                 calendarId=target_calendar_id,
-                                eventId=updated_event['id'],
-                                body=updated_event
+                                eventId=project_event['id'],
+                                body=event_row_to_body(project_event),
                             ).execute()
 
-                            # create new event starting at next free position and ending after time cut from original
-                            # event that is otherwise identical
-                            if 'description' in updated_event.keys():
-                                description = updated_event['description']
-                            else:
-                                description = ''
-                            calendar_service.create_events_in_windows(
-                                calendar_ids=calendar_lookup['calendar_id'].values,
-                                start_timestamp=split_timestamp,
-                                duration=duration_to_trim,
-                                target_event_summary=updated_event['summary'],
-                                target_event_description=description,
-                                target_calendar_id=target_calendar_id,
-                                feierabend=FEIERABEND,
+                    updated_projects.add(updated_event['summary'])
+                if 'colorId' in updated_event.index and updated_event['colorId'] == SPLIT_COLOR_ID:
+                    end_timestamp = calendar_service.local_datetime_from_string(updated_event['end']['dateTime'])
+                    feierabend_timestamp = calendar_service.get_local_datetime(end_timestamp.date(), FEIERABEND)
+                    split_timestamp = None
+                    if end_timestamp > feierabend_timestamp:
+                        split_timestamp = feierabend_timestamp
+                    else:
+                        interrupting_events = pd.DataFrame()
+                        for calendar_id in calendar_lookup['calendar_id'].values:
+                            events_to_append, sync_token = calendar_service.get_event_df_and_next_sync_token(
+                                calendar_id=calendar_id,
+                                time_max=updated_event['end']['dateTime'],
+                                time_min=updated_event['start']['dateTime'],
                             )
+                            interrupting_events = interrupting_events.append(events_to_append)
+                        interrupting_events = interrupting_events[
+                            (interrupting_events['id'] != updated_event['id']) &
+                            (interrupting_events['start'].apply(lambda start_dict: 'dateTime' in start_dict.keys()))
+                            ]
+                        if len(interrupting_events) > 0:
+                            split_timestamp = interrupting_events['start'].apply(
+                                calendar_service.extract_local_datetime_or_nat).min()
+                    if split_timestamp is not None:
+                        duration_to_trim = end_timestamp - split_timestamp
+
+                        # update event to end at split timestamp
+                        updated_event = event_row_to_body(updated_event)
+                        del updated_event['colorId']
+                        updated_event['end']['dateTime'] = split_timestamp.isoformat()
+                        calendar_service.service.events().update(
+                            calendarId=target_calendar_id,
+                            eventId=updated_event['id'],
+                            body=updated_event
+                        ).execute()
+
+                        # create new event starting at next free position and ending after time cut from original
+                        # event that is otherwise identical
+                        if 'description' in updated_event.keys():
+                            description = updated_event['description']
+                        else:
+                            description = ''
+                        calendar_service.create_events_in_windows(
+                            calendar_ids=calendar_lookup['calendar_id'].values,
+                            start_timestamp=split_timestamp,
+                            duration=duration_to_trim,
+                            target_event_summary=updated_event['summary'],
+                            target_event_description=description,
+                            target_calendar_id=target_calendar_id,
+                            feierabend=FEIERABEND,
+                        )
 
         # remember the last time we retrieved events for next update
         calendar_lookup.loc[channel_id, 'sync_token'] = sync_token
