732e326059c41153629822878788b9eede230a42
Lukas Loos <lukas.loos@sap.de>
2021-09-06 10:53:23 +0200

    Extract methods from all handler conditions and remove old script

--- calendar_handler.py
+++ calendar_handler.py
@@ -47,6 +47,7 @@ def log_request_info():
 class CalendarHandler(Resource):
     def __init__(self):
         self.next_sync_token = None
+        self.updated_projects = None
 
     @cached_property
     def calendar_lookup(self):
@@ -78,63 +79,72 @@ class CalendarHandler(Resource):
             updated_events = updated_events[updated_events['status'] == 'confirmed'].sort_values(
                 by='updated', ascending=False, ignore_index=True)
             # remember projects that were already updated to update each project only once
-            updated_projects = set()
+            self.updated_projects = set()
             for _, updated_event in updated_events.iterrows():
                 summary_args = shlex.split(updated_event['summary'])
-                # only trigger updates for events that belong to a project that hasn't been updated so far
-                if PROJECT_ARGUMENT in summary_args and updated_event['summary'] not in updated_projects:
-
-                    # get events that belong to this project
-                    project_events, self.next_sync_token = calendar_service.get_event_df_and_next_sync_token(
-                        calendar_id=source_calendar_id,
-                        query=updated_event['summary'][:-3],
-                    )
-
-                    # update all events in this project except the event that triggered the update
-                    new_description = updated_event['description']
-                    for _, project_event in project_events.iterrows():
-                        if project_event['id'] != updated_event['id'] and \
-                                project_event['summary'] == updated_event['summary'] and \
-                                project_event['description'] != new_description:
-                            project_event['description'] = new_description
-                            calendar_service.update_event(
-                                body=event_row_to_body(project_event), calendar_id=source_calendar_id)
-
-                    updated_projects.add(updated_event['summary'])
+                # Check if updated event belongs to project and the project hasn't been updated already
+                if PROJECT_ARGUMENT in summary_args and updated_event['summary'] not in self.updated_projects:
+                    self.update_project(source_calendar_id, updated_event)
 
                 # Check if event is supposed to be split (or moved)
                 if 'colorId' in updated_event.index and updated_event['colorId'] == SPLIT_COLOR_ID:
                     self.split_or_move_event(source_calendar_id, updated_event)
 
+                # Check if event is supposed to be moved to another calendar
                 if SWITCH_CALENDAR_ARGUMENT in summary_args:
-                    # create new event in target calendar
-                    target_calendar_summary = summary_args.pop(summary_args.index(SWITCH_CALENDAR_ARGUMENT) + 1)
-                    summary_args.remove(SWITCH_CALENDAR_ARGUMENT)
-                    optional_fields = {
-                        field: updated_event[field] for field in OPTIONAL_EVENT_FIELDS if field in updated_event.keys()}
-                    calendar_service.create_event(
-                        start=calendar_service.local_datetime_from_string(updated_event['start']['dateTime']),
-                        end=calendar_service.local_datetime_from_string(updated_event['end']['dateTime']),
-                        summary=shlex.join(summary_args),
-                        calendar_id=calendar_service.calendar_dict[target_calendar_summary],
-                        **optional_fields,
-                    )
-                    # remove event from source calendar
-                    calendar_service.delete_event(source_calendar_id, updated_event['id'])
+                    self.switch_calendar(source_calendar_id, summary_args, updated_event)
 
+                # Check if event is from work calendar and transparency is not set yet
                 if calendar_row['name'] == 'Arbeit' and (
                         'transparency' not in updated_event.index or
                         pd.isnull(updated_event['transparency'])
                 ):
-                    updated_event['transparency'] = 'transparent'
-                    calendar_service.update_event(
-                        body=event_row_to_body(updated_event), calendar_id=source_calendar_id)
+                    self.set_event_transparent(source_calendar_id, updated_event)
 
         # remember the last time we retrieved events for next update
         self.calendar_lookup.loc[channel_id, 'sync_token'] = self.next_sync_token
         self.calendar_lookup.to_csv(CALENDAR_LOOKUP_PATH)
         # invalidate calendar lookup cache
         del self.__dict__['calendar_lookup']
+
+    @staticmethod
+    def set_event_transparent(source_calendar_id, updated_event):
+        updated_event['transparency'] = 'transparent'
+        calendar_service.update_event(body=event_row_to_body(updated_event), calendar_id=source_calendar_id)
+
+    def update_project(self, source_calendar_id, updated_event):
+        # get events that belong to this project
+        project_events, self.next_sync_token = calendar_service.get_event_df_and_next_sync_token(
+            calendar_id=source_calendar_id,
+            query=updated_event['summary'][:-3],
+        )
+        # update all events in this project except the event that triggered the update
+        new_description = updated_event['description']
+        for _, project_event in project_events.iterrows():
+            if project_event['id'] != updated_event['id'] and \
+                    project_event['summary'] == updated_event['summary'] and \
+                    project_event['description'] != new_description:
+                project_event['description'] = new_description
+                calendar_service.update_event(
+                    body=event_row_to_body(project_event), calendar_id=source_calendar_id)
+        self.updated_projects.add(updated_event['summary'])
+
+    @staticmethod
+    def switch_calendar(source_calendar_id, summary_args, updated_event):
+        # create new event in target calendar
+        target_calendar_summary = summary_args.pop(summary_args.index(SWITCH_CALENDAR_ARGUMENT) + 1)
+        summary_args.remove(SWITCH_CALENDAR_ARGUMENT)
+        optional_fields = {
+            field: updated_event[field] for field in OPTIONAL_EVENT_FIELDS if field in updated_event.keys()}
+        calendar_service.create_event(
+            start=calendar_service.local_datetime_from_string(updated_event['start']['dateTime']),
+            end=calendar_service.local_datetime_from_string(updated_event['end']['dateTime']),
+            summary=shlex.join(summary_args),
+            calendar_id=calendar_service.calendar_dict[target_calendar_summary],
+            **optional_fields,
+        )
+        # remove event from source calendar
+        calendar_service.delete_event(source_calendar_id, updated_event['id'])
 
     def split_or_move_event(self, target_calendar_id, updated_event):
         start_timestamp, end_timestamp = (
