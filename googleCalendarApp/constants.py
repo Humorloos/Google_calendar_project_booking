4c5b016fb31b875ddb3f17c7ca21b098f78685a4
Humorloos <l.loos95@gmail.com>
2021-11-20 15:56:37 +0100

    Move credentials to user home

--- googleCalendarApp/constants.py
+++ googleCalendarApp/constants.py
@@ -4,7 +4,8 @@ MODULE_DIR = Path(__file__).parent.parent
 PROJECT_DIR = MODULE_DIR.parent
 RESOURCES_PATH = MODULE_DIR.joinpath('resources')
 CALENDAR_LOOKUP_PATH = RESOURCES_PATH.joinpath('calendar_lookup.csv')
-GOOGLE_API_PATH = RESOURCES_PATH.joinpath('google_api')
+
+GOOGLE_API_PATH = Path.home() / '.google_api'
 
 PROJECT_ARGUMENT = '-p'
 CALENDAR_URI = 'calendar'
